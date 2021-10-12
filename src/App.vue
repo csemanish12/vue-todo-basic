67db71fc6fc793f2f51b5bf92c2d45c8c222f0e9
Neha Pandey <sahmanish20@gmail.com>
2021-10-12 19:49:24 +0545

    add todo component

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <h1> Todo List</h1>
+  <ul>
+    <li v-for="item in TodoItems" :key="item.id">
+      <TodoItem :label="item.label" :done="item.done" :id="item.id"></TodoItem>
+    </li>
+  </ul>
+</template>
+
+<script>
+import TodoItem from './components/TodoItem.vue'
+
+export default {
+  name: 'App',
+  components: {
+    TodoItem
+  },
+  data(){
+    return {
+      TodoItems: [
+        {label: 'Learn Vue', done: false, id: 1},
+        {label: 'Create a Vue with cli', done: true, id: 2},
+        {label: 'Have fun', done: true, id: 3},
+        {label: 'Create a todo list', done: false, id:4}
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
