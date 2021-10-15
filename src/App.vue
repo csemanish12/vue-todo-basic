c0b7cb30f979fd250566cbcc5e840ecf5ea84090
Neha Pandey <sahmanish20@gmail.com>
2021-10-15 07:25:24 +0545

    create custom events

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,39 @@
 <template>
-  <h1> Todo List</h1>
-  <ul>
-    <li v-for="item in TodoItems" :key="item.id">
-      <TodoItem :label="item.label" :done="item.done" :id="item.id"></TodoItem>
-    </li>
-  </ul>
+  <div id="app">
+    <h1> Todo List</h1>
+    <TodoForm @todo-added="addTodo"></TodoForm>
+    <ul>
+      <li v-for="item in TodoItems" :key="item.id">
+        <TodoItem :label="item.label" :done="item.done" :id="item.id"></TodoItem>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
 import TodoItem from './components/TodoItem.vue'
+import TodoForm from  './components/TodoForm.vue'
 
 export default {
   name: 'App',
   components: {
-    TodoItem
+    TodoItem,
+    TodoForm
   },
   data(){
     return {
       TodoItems: [
-        {label: 'Learn Vue', done: false, id: 1},
-        {label: 'Create a Vue with cli', done: true, id: 2},
-        {label: 'Have fun', done: true, id: 3},
-        {label: 'Create a todo list', done: false, id:4}
+        {label: 'Learn Vue', done: false, id: "1"},
+        {label: 'Create a Vue with cli', done: true, id: "2"},
+        {label: 'Have fun', done: true, id: "3"},
+        {label: 'Create a todo list', done: false, id:"4"}
       ]
+    }
+  },
+  methods: {
+    addTodo(todoLabel){
+      console.log('Todo added:', todoLabel)
+      this.TodoItems.push({id:"5", label:todoLabel,done:false})
     }
   }
 }
